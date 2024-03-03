e0eb949f6fbeafd6cd3e817ba75b6157925fa7a7
LIMAMMohmedlimam <medlimamuia@gmail.com>
2024-03-03 17:10:01 +0100

    .

--- TP1/exo5.php
+++ TP1/exo5.php
@@ -10,10 +10,11 @@ $sql = "SELECT id, prenom, nom, age, email FROM user";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // entaite tableau
+    // entête tableau
+    echo "<h1>Liste des utilisateurs</h1>";
     echo '<table border="3" style="width: 50%; height: 25%"><tr><th>ID</th><th>Prénom</th><th>Nom</th><th>Âge</th><th>Email</th></tr>';
     
-    // les donnés 
+    // les données 
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["prenom"]. "</td><td>" . $row["nom"]. "</td><td>" . $row["age"]. "</td><td>" . $row["email"]. "</td></tr>";
     }
